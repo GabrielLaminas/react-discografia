7b1b1bbdd8f4873fc9fd131bf4206e2ad5ec0d3d
Gabriel de Freitas Laminas <laminas533@gmail.com>
2022-05-10 19:01:28 -0300

    adicionando style no header componente

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './App.css';
+
 import Button from './Components/Button';
 import Search from './Components/Search';
 import Header from './Layout/Header';
 
 const App = () => {
   return (
-    <main>
+    <main className='main__container'>
       <Header />
       <Search />
       <Button name="Procurar" />
--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,18 +1,20 @@
 import React from 'react';
-
+import './Header.css'
 import logo from '../logoTiaoCarreiro.png';
 
 const Header = () => {
   return (
-    <header>
-      <div>
+    <header className='header__menu'>
+      <div className='header__logo'>
         <img 
           src={logo} 
           alt="logo com imagem de Tião Carreiro" 
         />
       </div>
 
-      <h1>Discografia</h1>
+      <h1 className='header__title'>
+        Discografia
+      </h1>
     </header>
   )
 }
